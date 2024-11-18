e594b3dab0f35003cba0a1ccfae62572c598567e
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-18 17:27:20 -0600

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Header from './components/header';
 import About from './components/About';
 import Contact from './components/Contact';
 
+npm create vite@latest my-vue-app -- --template react
 function App() {
   return (
     <div className="App">
