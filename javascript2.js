461db91e3380f11bd6d55d5b5ae193585934fa01
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-18 09:47:15 -0600

    Update javascript2.js

--- javascript2.js
+++ javascript2.js
@@ -1,10 +1,4 @@
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
+
 
 export default function App() {
     return (
