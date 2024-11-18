6df1497f8d322341e5c817cf8344e81a2eaf2353
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-18 17:23:29 -0600

    Create About.js

---
+++ About.js
@@ -0,0 +1,19 @@
+// About.js
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section className="about-section">
+      <h2>About Me</h2>
+      <div className="about-content">
+        <p>
+          Hello! I'm a budding software developer currently learning React. React is awesome and this 
+          assignment was pretty easy and enjoyable.  
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
